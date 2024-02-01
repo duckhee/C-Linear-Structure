a574ee6e213f4407da9b9915459ad135605e96c3
DoukheeWon <fain9301@yahoo.com>
2024-02-01 23:12:44 +0900

    feat: signle list basic add

---
+++ chapter01/ex01-arrayBaseLinearStructure/dataStructure.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef struct _UserData {
+    int age;
+    char name[32];
+    char phone[32];
+    struct _UserData *pNext;
+} UserData;
+
+int main() {
+    UserData a = {10, "hoon", "010-1234-1234"};
+    UserData b;
+    b = a;
+    printf("%d. %s. %s.\r\n", b.age, b.name, b.phone);
+    UserData userLists[4] = {
+            {20, "tester01", "010-1234-1234", NULL},
+            {21, "tester02", "010-1234-2222", NULL},
+            {22, "tester03", "010-1234-3333", NULL},
+            {23, "tester04", "010-1234-4444", NULL},
+    };
+    /** 연결 리스트 형태로 만들기 위한 link */
+    userLists[0].pNext = &userLists[1];
+    userLists[1].pNext = &userLists[2];
+    userLists[2].pNext = &userLists[3];
+    userLists[3].pNext = NULL;
+    /** 연결에 대한 링크 과정을 이해하기 위한 이름 출력 */
+    printf("%s\r\n", userLists[0].name);
+    printf("%s\r\n", userLists[0].pNext->name);
+    printf("%s\r\n", userLists[0].pNext->pNext->name);
+    printf("%s\r\n", userLists[0].pNext->pNext->pNext->name);
+
+    /** 연결 리스트를 출력 하는 방법 */
+    UserData *cur = NULL;
+    cur = &userLists[0];
+    while (cur != NULL) {
+        printf("%d, %s. %s.\r\n", cur->age, cur->name, cur->phone);
+        cur = cur->pNext;
+    }
+
+
+    return 0;
+}
