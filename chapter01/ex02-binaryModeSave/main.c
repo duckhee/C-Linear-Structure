516a8e71a4999c0eaad835961c26ed2774170c6a
DoukheeWon <fain9301@yahoo.com>
2024-02-02 15:42:06 +0900

    feat: file binary mode save and read binary file

--- chapter01/ex02-binaryModeSave/main.c
+++ chapter01/ex02-binaryModeSave/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-
+/** 구조체 저장에 최적화를 하기 위한 전처리 선언 #pragma pack(push,1) #pragma pack(pop) */
 //#pragma pack(push, 1)
 typedef struct _UserData {
 
